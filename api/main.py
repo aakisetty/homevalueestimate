dd1a67d0a239746e04306d24640c2645064fcf38
meaLuda <munyalamea@gmail.com>
2024-08-01 21:46:25 +0300

    main update

--- api/main.py
+++ api/main.py
@@ -95,6 +95,6 @@ async def root():
     """
     return {"message": "Welcome to the Home Value Estimator API"}
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run("main:app", host="0.0.0.0", port=8000,reload=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,reload=True)
